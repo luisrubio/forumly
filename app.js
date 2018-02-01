82c8f96a51a7537f115834597e2d7c11942e21f4
Luis <basic@ubuntu.ubuntu-domain>
2018-02-01 13:41:16 -0600

    feat: Add post and new routes for topics

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render('index');
+  res.render("index");
 });
 
+// **********************
+// Topics route
+// **********************
+var topics = [
+  {title: 'Topic1', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'},
+  {title: 'Topic2', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'},
+  {title: 'Topic3', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'}
+];
+
 app.get("/topics", (req, res) => {
-  var topics = [
-    {title: 'Topic1', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'},
-    {title: 'Topic2', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'},
-    {title: 'Topic3', image: 'https://image.prntscr.com/image/Ob-hRdc_SSq67FClWr5qFw.jpeg'}
-  ];
-  res.render('topics/index', {topics:topics});
+  res.render("topics/index", {topics:topics});
+});
+
+app.get("/topics/new", function(req, res){
+  res.render("topics/new");
+});
+
+app.post("/topics", (req, res) => {
+  const title = req.body.title;
+  const image = req.body.image;
+  var newTopic = {title: title, image: image};
+  topics.push(newTopic);
+  res.redirect("/topics");
 });
 
 app.get("/topics/:id", (req, res) => {
