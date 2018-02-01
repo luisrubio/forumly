b62bd119f2782251fb2a6df531da688113f99832
Luis <basic@ubuntu.ubuntu-domain>
2018-02-01 17:42:16 -0600

    feat: Add authentication

--- app.js
+++ app.js
@@ -2,24 +2,42 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const Topic = require("./models/topic");
 const Comment = require("./models/comment");
+const User = require("./models/user");
 const seedDB = require('./seeds');
+
 
 mongoose.connect("mongodb://lulu:lulu@ds263847.mlab.com:63847/teriyakirubi");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+seedDB();
+
+// PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: "this is a secret message",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-
-seedDB();
-
-// Topics Schema Setup
-
 
 // **********************
 // Topics route
@@ -27,6 +45,7 @@ seedDB();
 
 // INDEX
 app.get("/topics", (req, res) => {
+  // get topics from DB
   Topic.find({}, function(err, allTopics){
     if(err){
       console.log(err);
@@ -76,7 +95,7 @@ app.get("/topics/:id", (req, res) => {
 // comments route
 // **********************
 
-app.get("/topics/:id/comments/new", (req, res) => {
+app.get("/topics/:id/comments/new", isLoggedIn, (req, res) => {
   // find topic by id
   Topic.findById(req.params.id, (err, topic) => {
     if(err) {
@@ -87,7 +106,7 @@ app.get("/topics/:id/comments/new", (req, res) => {
   })
 });
 
-app.post("/topics/:id/comments", (req, res) => {
+app.post("/topics/:id/comments", isLoggedIn, (req, res) => {
   // find topic by id
   Topic.findById(req.params.id, (err, topic) => {
     if(err){
@@ -106,4 +125,54 @@ app.post("/topics/:id/comments", (req, res) => {
     }
   });
 });
+
+// **********************
+// auth route
+// **********************
+
+// show register form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// handle sign up
+app.post("/register", (req, res) => {
+  const newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/topics");
+    });
+  });
+});
+
+// show login form
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// handle Login
+app.post("/login", passport.authenticate("local",
+  {
+    successRedirect: "/topics",
+    failureRedirect: "/login"
+  }), (req, res) => {
+});
+
+// handle logout
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/topics");
+});
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
 app.listen(3000, '0.0.0.0', () => console.log("Forumly started."));
