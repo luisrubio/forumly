f46f661f416f08b5ab0f6e8192f8c27b3b0835b0
luis-rubio <luisrubio8@gmail.com>
2018-02-03 17:20:40 -0600

    feat: Add delete and edit CRUD routes for comments

--- routes/comments.js
+++ routes/comments.js
@@ -42,12 +42,66 @@ router.post("/", isLoggedIn, (req, res) => {
   });
 });
 
+// EDIT
+router.get("/:comment_id/edit", checkCommentOwner, (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if(err){
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {topic_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// UPDATE
+router.put("/:comment_id", checkCommentOwner, (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/topics/" + req.params.id);
+    }
+  });
+});
+
+// DESTROY
+router.delete("/:comment_id", checkCommentOwner, (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/topics/" + req.params.id)
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwner(req, res, next) {
+  if(req.isAuthenticated()) {
+
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+        res.redirect("back");
+      } else {
+        // do they own comment?
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        // if not owner
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
